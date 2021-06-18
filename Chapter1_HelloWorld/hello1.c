57993737298f40655595d1f527331caea9ce027a
cooldoggy <yoyoboy30@outlook.com>
2021-06-18 01:02:25 -0400

    Initial Commit - Chapter 1 complete

---
+++ Chapter1_HelloWorld/hello1.c
@@ -0,0 +1,14 @@
+/*
+ * hello2.c
+ * My first C program with comments.
+ * by Erin R.
+ * created 2021/05/16
+ */
+#include <stdio.h>
+
+int main()
+{
+	printf( "Hello, World!\n");
+	return 0;
+}
+/* eof */
